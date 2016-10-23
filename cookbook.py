f2dbdd1f400b2a467ff0ee81884b77a0013e639a
Michael Pusterhofer <pusterhofer@student.tugraz.at>
2016-10-24 01:43:38 +0200

    * add tags * add part of ingredients

--- cookbook.py
+++ cookbook.py
@@ -1,59 +1,70 @@
 #!/bin/env python
 
 import gi
+
 gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk
 from sqlalchemy import *
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker,relationship
+from sqlalchemy.orm import sessionmaker, relationship
 
-#--------------------------------------------------------
+# --------------------------------------------------------
 # Models
-#--------------------------------------------------------
+# --------------------------------------------------------
 Base = declarative_base()
 
 a_table_tags = Table('a_recipe_tags', Base.metadata,
-    Column('recipes_id', Integer, ForeignKey('recipes.id')),
-    Column('tags_id', Integer, ForeignKey('tags.id'))
-)
+                     Column('recipes_id', Integer, ForeignKey('recipes.id')),
+                     Column('tags_id', Integer, ForeignKey('tags.id'))
+                     )
+a_table_ingredients = Table('a_recipe_ingredients', Base.metadata,
+                     Column('recipes_id', Integer, ForeignKey('recipes.id')),
+                     Column('ingredients_id', Integer, ForeignKey('ingredients.id'))
+                     )
+
 
 class Recipe(Base):
     __tablename__ = "recipes"
-    id = Column(Integer,primary_key=True)
-    name = Column(String,default="Neues Rezept",unique=True)
-    baking_time = Column(String,default="")
-    baking_temp = Column(String,default="")
-    description = Column(String,default="")
+    id = Column(Integer, primary_key=True)
+    name = Column(String, default="Neues Rezept", unique=True)
+    baking_time = Column(String, default="")
+    baking_temp = Column(String, default="")
+    description = Column(String, default="")
     picture = Column(LargeBinary)
     category_id = Column(Integer, ForeignKey('categories.id'))
     category = relationship("Category")
-    tags = relationship("Tags",secondary=a_table_tags)
+    tags = relationship("Tags", secondary=a_table_tags)
+    ingredients = relationship("Ingredients", secondary=a_table_ingredients)
 
 
 class Category(Base):
     __tablename__ = "categories"
-    id = Column(Integer,primary_key=True)
-    name = Column(String,unique=True)
+    id = Column(Integer, primary_key=True)
+    name = Column(String, unique=True)
+
 
 class Tags(Base):
     __tablename__ = "tags"
-    id = Column(Integer,primary_key=True)
-    name = Column(String,default="")
+    id = Column(Integer, primary_key=True)
+    name = Column(String, default="")
+
 
 class Ingredients(Base):
     __tablename__ = "ingredients"
-    id = Column(Integer,primary_key=True)
+    id = Column(Integer, primary_key=True)
     name = Column(String)
     amount = Column(String, default="")
 
+
 class RecipeRow(Gtk.ListBoxRow):
-    def __init__(self,recipe):
+    def __init__(self, recipe):
         super(Gtk.ListBoxRow, self).__init__()
         self.recipe = recipe
         self.add(Gtk.Label(recipe.name))
 
+
 class Handler:
-    def add_recipe(self,button):
+    def add_recipe(self, button):
         try:
             recipe = Recipe()
             session.add(recipe)
@@ -75,13 +86,11 @@ class Handler:
         for c in cats:
             ls_cat.append([c.name])
 
-
-
-    def load_recipe(self,selection):
-        model,item = selection.get_selected()
+    def load_recipe(self, selection):
+        model, item = selection.get_selected()
         if not item:
             return
-        recipe_name = model.get_value(item,0)
+        recipe_name = model.get_value(item, 0)
         if not recipe_name:
             return
         global current_recipe
@@ -100,27 +109,27 @@ class Handler:
 
         tags = builder.get_object("txt_tags")
         if current_recipe.tags:
-            tags.set_text(",".join(map(lambda t:t.name,current_recipe.tags)))
+            tags.set_text(",".join(map(lambda t: t.name, current_recipe.tags)))
         else:
             tags.set_text("")
 
         desc = builder.get_object("txt_description")
         desc.get_buffer().set_text(current_recipe.description)
 
-    def save_recipe(self,button):
+    def save_recipe(self, button):
         name = builder.get_object("txt_name")
         time = builder.get_object("txt_time")
         temp = builder.get_object("txt_temp")
         desc = builder.get_object("txt_description")
         dbuf = desc.get_buffer()
-        current_recipe.description = dbuf.get_text(dbuf.get_start_iter(),dbuf.get_end_iter(),False)
+        current_recipe.description = dbuf.get_text(dbuf.get_start_iter(), dbuf.get_end_iter(), False)
 
         tags = builder.get_object("txt_tags")
         tags = tags.get_text()
         tags = tags.split(",")
         current_recipe.tags.clear()
         for tag in tags:
-            db_tag = session.query(Tags).filter(Tags.name==tag).first()
+            db_tag = session.query(Tags).filter(Tags.name == tag).first()
             if db_tag:
                 current_recipe.tags.append(db_tag)
             else:
@@ -131,13 +140,17 @@ class Handler:
         cat = builder.get_object("txt_cat")
         cat_txt = cat.get_text()
         if cat_txt:
-            db_cat = session.query(Category).filter(Category.name==cat_txt).first()
+            db_cat = session.query(Category).filter(Category.name == cat_txt).first()
             if db_cat:
                 current_recipe.category = db_cat
             else:
-                new_cat = Category(name = cat_txt)
+                new_cat = Category(name=cat_txt)
                 session.add(new_cat)
                 current_recipe.category = new_cat
+
+        
+        ingredients = builder.get_object("ls_ingredients")
+        ingredients.foreach(print)
 
         current_recipe.name = name.get_text()
         current_recipe.baking_temp = temp.get_text()
@@ -145,17 +158,41 @@ class Handler:
         session.commit()
         self.update()
 
-    def show_main(self,widget):
+    def show_main(self, widget):
         cbt_cat = builder.get_object("cbt_cat")
         ls_cat = builder.get_object("ls_cat")
         cbt_cat.set_model(ls_cat)
 
         self.update()
 
-    def delete_recipe(self,button):
+    def delete_recipe(self, button):
         session.delete(current_recipe)
         session.commit()
         self.update()
+
+    def add_ingredient(self,button):
+        ingredients = builder.get_object("ls_ingredients")
+        ingredients.append(["-","-"])
+            
+    def remove_ingredient(self,button):
+        lv = builder.get_object("lv_ingredients")
+        selection = lv.get_selection()
+        if not selection:
+            return
+        model,path = selection.get_selected_rows()
+        if not path:
+            return
+        for p in path:
+            it = model.get_iter(path)
+            model.remove(it)
+
+    def edited_amount(*params):
+        print(params)
+
+
+
+        
+        
 
 
 current_recipe = None
